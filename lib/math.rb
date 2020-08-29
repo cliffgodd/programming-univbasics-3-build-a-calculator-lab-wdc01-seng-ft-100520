68db624b0523557d984800f816b370a715feffb5
Justin Clifford <justoon7.jc@gmail.com>
2020-08-29 08:01:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,3 +3,32 @@ def addition(num1, num2)
   num2 = 4
   return num2 + num1
 end
+
+def subtraction(num1, num2)
+  num1 = 10
+  num2 = 5
+  return num2 - num1
+end
+
+def division(num1, num2)
+  num1 = 50
+  num2 = 2
+  return num2 / num1
+end
+
+def multiplication(num1, num2)
+  num1 = 4
+  num2 = 30
+  return num1 * num2
+end
+
+def modulo(num1, num2)
+  num1 = 34
+  num2 = 5
+  return num2 % num1
+end
+
+def square_root(num)
+  num = 81
+  return num ** 2
+end
