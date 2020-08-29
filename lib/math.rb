f8209969d74e768268a4f4bffdd1df84e76510bb
Justin Clifford <justoon7.jc@gmail.com>
2020-08-29 07:50:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,7 @@
-def addition(num1, num2)
-
+def addition(5, 4)
+  puts 5 + 4
 end
 
-def subtraction(num1, num2)
 
-end
 
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
+addition
